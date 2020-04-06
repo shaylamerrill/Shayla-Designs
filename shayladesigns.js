11db578e5da0aea505937200252d224ba41c82d7
shaylamerrill <61173669+shaylamerrill@users.noreply.github.com>
2020-04-06 17:32:39 -0400

    first commit

---
+++ shayladesigns.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+  $("#multiskilled").mouseenter(function(){
+    $(".list").show(300);
+  });
+});
+
+$(document).ready(function(){
+  $("#multiskilled").mouseleave(function(){
+    $(".list").hide(300);
+  });
+});
+
+
+$(document).ready(function(){
+  $(".location").mouseenter(function(){
+    $(".wrapper").show(300);
+  });
+});
+
+$(document).ready(function(){
+  $(".location").mouseleave(function(){
+    $(".wrapper").hide(300);
+  });
+});
+
