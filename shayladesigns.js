567a706e681ffbae1242a32359bc79bf944dc41f
shaylamerrill <61173669+shaylamerrill@users.noreply.github.com>
2020-04-15 01:47:12 -0400

    bubbles

--- shayladesigns.js
+++ shayladesigns.js
@@ -1,26 +1,159 @@
 
-$(document).ready(function(){
-  $("#multiskilled").mouseenter(function(){
-    $(".list").show(300);
-  });
+$(document).ready(function(){ 
+
+  /* circles start */
+jQuery(function($) {
+   $('#circle1').mouseover(function() {
+       //var dWidth = $(document).width() - 100, //  100 = image width
+           //dHeight = $(document).height() - 100, //  100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle2').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle3').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle4').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle5').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle6').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+    $('#circle7').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle8').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle9').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle10').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle11').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle12').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+        $('#circle13').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle14').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
+   $('#circle15').mouseover(function() {
+       //var dWidth = $(document).width() - 100, // 100 = image width
+           //dHeight = $(document).height() - 100, // 100 = image height
+        var nextX = Math.floor(Math.random() * ($(this).parent().width() - $(this).width() + 1) + 1),
+           nextY = Math.floor(Math.random() * ($(this).parent().height() - $(this).height() - 1) + 1);
+       $(this).animate({ right: nextX + 'px', top: nextY + 'px' });
+   });
+
 });
 
-$(document).ready(function(){
-  $("#multiskilled").mouseleave(function(){
-    $(".list").hide(300);
-  });
 });
 
+/* circles end */
+
+
+
+
 
 $(document).ready(function(){
-  $(".location").mouseenter(function(){
-    $(".wrapper").show(300);
+    $("#multiskilled").mouseenter(function(){
+      $(".list").show(300);
+    });
   });
-});
-
-$(document).ready(function(){
-  $(".location").mouseleave(function(){
-    $(".wrapper").hide(300);
+  
+  $(document).ready(function(){
+    $("#multiskilled").mouseleave(function(){
+      $(".list").hide(300);
+    });
   });
-});
-
+  
+  
+  $(document).ready(function(){
+    $(".location").mouseenter(function(){
+      $(".wrapper").show(300);
+    });
+  });
+  
+  $(document).ready(function(){
+    $(".location").mouseleave(function(){
+      $(".wrapper").hide(300);
+    });
+  });
